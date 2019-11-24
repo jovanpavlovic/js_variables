4fb431f75d9bcede3d9b34f5053a18f0473473ee
jovanpavlovic <jovan94pavlovic@gmail.com>
2019-11-24 23:28:29 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// TASK 1
+
+var petersMoney, jimsMoney;
+
+petersMoney = 3500;
+jimsMoney = 5000;
+
+console.log(petersMoney>jimsMoney);
+
+// TASK 2
+
+var johnsAge, futureYears, mikesAge;
+
+johnsAge = 38;
+futureYears = 8;
+mikesAge = (johnsAge + futureYears)/2 - futureYears;
+console.log("Mike is " + mikesAge + " years old now.");
+
+// TASK 3
+
+var firstFlock, secondFlock, firstMigration, secondMigration;
+
+firstFlock = 78;
+secondFlock = 57;
+firstMigration = 12;
+secondMigration = 4;
+
+firstFlock -= firstMigration;
+secondFlock += firstMigration;
+secondFlock -= secondMigration;
+
+console.log(firstFlock >= secondFlock);
