2cef0652436e0c3722cc23b26b60f5c61d45ec24
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2019-12-17 23:10:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -114,3 +114,96 @@ var printNums = function(nums, singleNum) {
   console.log(biggerNum);
 }
 printNums([3, 76 ,67, 2], 6);
+
+// //TASK 6
+
+console.log("TASK 6");
+
+function largestNum(arr) {
+
+  var highestNumber = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > highestNumber) {
+      highestNumber = arr[i];
+    }
+  }
+
+  return highestNumber;
+}
+
+function lowestNum(arrTwo) {
+
+  var lowestNumber = arrTwo[0];
+
+  for (var i = 1; i < arrTwo.length; i++) {
+    if (arrTwo[i] < lowestNumber) {
+      lowestNumber = arrTwo[i];
+    }
+  }
+
+  return lowestNumber;
+}
+
+function multiply(functionLargest, functionLowest) {
+
+  someArr = [0,1,2,3,4,5,-1];
+
+  var highestNumber = functionLargest(someArr);
+  var lowestNumber = functionLowest(someArr);
+
+  console.log("Lowest number * Highest number from this array: (" + someArr +  ") = " + highestNumber * lowestNumber);
+}
+
+multiply(largestNum, lowestNum);
+
+//TASK 7
+
+console.log("TASK 7");
+
+var z = [15,35,46,23,15,17,23,24,35,12,72,64,35,22,64];
+
+var deleteLastOne = function(arr){
+  arr.length = arr.length - 1;
+}
+
+var newArr = function(arr, arrTwo){
+  for (var i = 0; i < arr.length; i++) { 
+    arrTwo[i] = arr[i];
+  }
+}
+
+var uniqueValues = function(arr, arrThree){ 
+  var p = 0;          
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
+      arrThree[p] = arr[i];
+      p++;
+    }
+  }
+}
+
+function sortByAscending(arr)  {  
+for (var i = 1; i < arr.length; i++)
+    for (var j = 0; j < i; j++)
+        if (arr[i] < arr[j]) {
+          var s = arr[i];
+          arr[i] = arr[j];
+          arr[j] = s;
+        }
+}
+
+var uniqueNumbers = function(arr, data){
+    if (arr.length > 1) {
+      var y = [];
+      var x = [];
+
+      newArr(z, x);
+      sortByAscending(x);
+      uniqueValues(x, y);
+      data(y);
+      console.log(y);
+  } 
+}
+
+uniqueNumbers(z, deleteLastOne);
