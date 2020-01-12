3e2fa3d64518eb030696bc932f705a34df97764d
jovanpavlovic <jovan94pavlovic@gmail.com>
2020-01-12 21:00:09 +0100

    eight homework

--- js/main.js
+++ js/main.js
@@ -1,216 +1,107 @@
-
-// TASK 1
+//TASK 1 
 
 console.log("Task 1");
 
-var sentence = "Lorem ipsum dolor sit amet";
-
-function lowercase(lowerSentence) {
-  return lowerSentence.toLowerCase();
+function Student(name,surname,age,avg){
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+  this.avg = avg;
 }
 
-sentence = lowercase(sentence);
+var mike = new Student("Mike", "Williams", 25, 7.8);
+var peter = new Student("Peter", "Johnson", 23, 8.9);
 
-console.log(sentence);
+console.log(mike, peter);
 
-// TASK 2
+//TASK 2
 
 console.log("Task 2");
 
-var sentenceTwo = "Lorem ipsum dolor sit amet";
+function FootbalPlayer(name,age,goals,yellowCards){
+  this.name = name;
+  this.age = age;
+  this.goals = goals;
+  this.yellowCards = yellowCards;
+}
 
-function check (checkString) {
-  if (checkString.includes("sit")) {
-    console.log("The word *sit* exist in a string");
-  } 
-  else{
-    console.log("The word *sit* exist in a string")
+FootbalPlayer.prototype.team = "Juventus";
+
+var cristiano = new FootbalPlayer("Cristiano",34, 10, 0);
+var paulo = new FootbalPlayer("Paulo",26, 8, 3);
+var matthijs = new FootbalPlayer("Matthijs",20, 1, 2);
+var adrien = new FootbalPlayer("Adrien",24, 0, 2);
+var merih = new FootbalPlayer("Merih",21, 1, 1);
+
+console.log(cristiano,paulo,matthijs,adrien,merih);
+
+//TASK 3
+
+console.log("Task 3")
+
+function Player(name,age,gender,adress){
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+}
+
+Player.prototype.team = "Juventus";
+
+var footbalPlayers = [
+["Cristiano",34, 10, 0],
+["Paulo",26, 8, 3],
+["Matthijs",20, 1, 2],
+["Adrien",24, 0, 2],
+["Merih",21, 1, 1],
+];
+
+function write(footbalPlayers){
+  var newplayers = [];
+  for (var i = 0; i < footbalPlayers.length; i++) {
+    newplayers = new Player(footbalPlayers[i][0], footbalPlayers[i][1], footbalPlayers[i][2], footbalPlayers[i][3]);
+    console.log(newplayers);
   }
 }
+write(footbalPlayers);
 
-check(sentenceTwo);
+//TASK 4
 
-// TASK 3
+console.log("TASK 4");
 
-console.log("Task 3");
-
-var sentenceThree = "Lorem ipsum dolor sit amet";
-
-function findindex (check) {
-  return check.length - 1;
+function Method(firstNum, secondNum, method) {
+  this.firstNum = firstNum;
+  this.secondNum = secondNum;
+  this.result = function() {
+    return method(this.firstNum, this.secondNum);
+  };
 }
 
-sentenceThree = findindex(sentenceThree);
-
-console.log(sentenceThree);
-
-// TASK 4
-
-console.log("Task 4");
-
-var sentenceFour = "Lorem ipsum dolor sit amet";
-
-function cutString (cutSentence) {
-  return cutSentence.split(" ",3) 
+function add(firstNum, secondNum) {
+  var x = firstNum + secondNum;
+  return x;
+}
+function substract(firstNum, secondNum) {
+  var x = firstNum - secondNum;
+  return x;
+}
+function multiply(firstNum, secondNum) {
+  var x = firstNum * secondNum;
+  return x;
+}
+function divide(firstNum, secondNum) {
+  var x = firstNum / secondNum;
+  return x;
 }
 
-sentenceFour = cutString(sentenceFour);
 
-console.log(sentenceFour);
+var addition = new Method(11, 9, add);
+console.log("11 + 9 = " + addition.result());
 
+var substraction = new Method(8, 7, substract);
+console.log("8 - 7 = " + substraction.result());
 
-// TASK 5
+var multiplication = new Method(9, 5, multiply);
+console.log("9 * 5 = " +multiplication.result());
 
-console.log("Task 5");
-
-var sentenceFive = "Piter is an actor.";
-
-function replaceString(str){
-  str = str.split("");
-  str[3] = "o";
-  str[15] = "e";
-  return str = str.join("");
-}
-
-sentenceFive = replaceString(sentenceFive);
-
-console.log(sentenceFive);
-
-// TASK 6
-
-console.log("Task 6");
-
-var someData = [34, 23, 14, 56, 23, 44, 65];
-var number = 56;
-
-function newData(arr, num) {
-  arr = arr.filter(function(data){
-    return data !== num;
-  });
-  return arr;
-}
-
-someData = newData(someData,number);
-console.log(someData);
-
-// TASK 7
-
-console.log("Task 7");
-
-var someNewData = [34, 23, 14, 56, 23, 44, 65];
-
-function newArr(data){
-  var firstArr = data.slice(1,4)
-  var secondArr = data.splice(4,6)
-  var newArray = [...firstArr, ...secondArr.reverse()];
-  return newArray;
-}
-
-someNewData = newArr(someNewData)
-
-console.log(someNewData);
-
-
-// TASK 8
-
-console.log("Task 8");
-
-var data = [334, 233, 212, 199, 154, 122];
-
-function subtraction(arr) {
-
-  var newArray = arr.map(function(element){
-
-    if (arr.indexOf(element) !== 0) {
-
-      return element - arr[arr.indexOf(element)-1];
-
-    } else return element;
-  });
-
-  return newArray;
-}
-
-var newArr = subtraction(data);
-
-console.log(newArr);
-
-// Other way
-
-//var arr = [];
-// function reduce(reducedArr){
-//  for (var i = 0; i < reducedArr.length; i++) {
-//    arr[i] = reducedArr[i] - reducedArr[i - 1]
-//  }
-//  return arr;
-// }
-// reduce(data);
-// console.log(arr);
-
-// TASK 9
-
-console.log("Task 9");
-
-var students = [
-  {
-     name: "Jim",
-     avgGrade: 8.5556
-  },
-  {
-     name: "Mike",
-     avgGrade: 8.5492
-  },
-  {
-     name: "Anna",
-     avgGrade: 8.9322
-  },
-  {
-     name: "Jack",
-     avgGrade: 8.6111
-  }
-]
-
-var averageGrade = 8.5;
-
-function bestOf(arr, avg) {
-
-  var newArray = arr.filter(function(element){
-
-    return element.avgGrade > avg;
-
-  });
-
-  newArray.forEach(function(element) {
-
-    element.avgGrade = element.avgGrade.toFixed(2);
-
-  });
-
-  return newArray;
-}
-
-var goodStudents = bestOf(students, averageGrade);
-
-console.log(goodStudents);
-
-
-//Other way
-
-// var someArr = [];
-
-// function grade (student, grade) {
-//  for (var i = 0; i < student.length; i++) {
-//    if (student[i].avgGrade > grade) {
-//      someArr[i] = student[i].name + "`s" + " average grade is: " +  student[i].avgGrade.toFixed(2);
-//    } 
-//    else
-//    {
-//      console.log("There are no students with avg grade above 8.5");
-//    }
-
-//  }
-//  return someArr;
-// }
-
-// grade(students, averageGrade);
-// console.log(someArr);
+var division = new Method(45, 5, divide);
+console.log("45 / 5  = " +  division.result());
