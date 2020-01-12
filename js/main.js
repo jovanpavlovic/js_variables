9e823eb63531ddd1cdfe4c30b712289b234e71ce
jovanpavlovic <jovan94pavlovic@gmail.com>
2020-01-12 20:51:04 +0100

    Predefined functions

--- js/main.js
+++ js/main.js
@@ -1,210 +1,216 @@
-//TASK 1
 
-console.log("TASK 1")
+// TASK 1
 
-var person = {
-  name: "Mike",
-  age: 28,
-  married: true
+console.log("Task 1");
+
+var sentence = "Lorem ipsum dolor sit amet";
+
+function lowercase(lowerSentence) {
+  return lowerSentence.toLowerCase();
 }
 
-function changeAge(personData) {
-  personData.age = 34;
-  delete personData.married;
-}
+sentence = lowercase(sentence);
 
-changeAge(person);
+console.log(sentence);
 
-console.log(person);
+// TASK 2
 
-//TASK 2
+console.log("Task 2");
 
-console.log("TASK 2")
+var sentenceTwo = "Lorem ipsum dolor sit amet";
 
-var person_two = {
-  name: "Jack",
-  age: 32,
-  married: true
-  //children:[]  //uncomment for first solution
-}
-
-//First solution
-
-function check(object) {
-
-  if ("children" in object){
-    object.children = [{
-      name: "Anna",
-      age: 12,
-      gender: "Female"
-    },
-
-    {
-      name: "Marta",
-      age: 7,
-      gender: "Female"
-    }];
+function check (checkString) {
+  if (checkString.includes("sit")) {
+    console.log("The word *sit* exist in a string");
+  } 
+  else{
+    console.log("The word *sit* exist in a string")
   }
-
-  else {
-    object.children = [{
-      name: "Peter",
-      age: 3,
-      gender: "Male"
-    },
-
-    {
-      name: "Mike",
-      age: 9,
-      gender: "Male"
-    }];
-  }
-
 }
 
-//Second solution
-//  function check(object) {
-//   if (object!=children) {
-//     object.children = [{
-//       childName: "Peter",
-//       childAge: 3,
-//       childGender: "Male"
-//     },
+check(sentenceTwo);
 
-//     {
-//       childName: "Anna",
-//       childAge: 12,
-//       childGender: "Female"
-//     }];
-    
-//   }
+// TASK 3
+
+console.log("Task 3");
+
+var sentenceThree = "Lorem ipsum dolor sit amet";
+
+function findindex (check) {
+  return check.length - 1;
+}
+
+sentenceThree = findindex(sentenceThree);
+
+console.log(sentenceThree);
+
+// TASK 4
+
+console.log("Task 4");
+
+var sentenceFour = "Lorem ipsum dolor sit amet";
+
+function cutString (cutSentence) {
+  return cutSentence.split(" ",3) 
+}
+
+sentenceFour = cutString(sentenceFour);
+
+console.log(sentenceFour);
+
+
+// TASK 5
+
+console.log("Task 5");
+
+var sentenceFive = "Piter is an actor.";
+
+function replaceString(str){
+  str = str.split("");
+  str[3] = "o";
+  str[15] = "e";
+  return str = str.join("");
+}
+
+sentenceFive = replaceString(sentenceFive);
+
+console.log(sentenceFive);
+
+// TASK 6
+
+console.log("Task 6");
+
+var someData = [34, 23, 14, 56, 23, 44, 65];
+var number = 56;
+
+function newData(arr, num) {
+  arr = arr.filter(function(data){
+    return data !== num;
+  });
+  return arr;
+}
+
+someData = newData(someData,number);
+console.log(someData);
+
+// TASK 7
+
+console.log("Task 7");
+
+var someNewData = [34, 23, 14, 56, 23, 44, 65];
+
+function newArr(data){
+  var firstArr = data.slice(1,4)
+  var secondArr = data.splice(4,6)
+  var newArray = [...firstArr, ...secondArr.reverse()];
+  return newArray;
+}
+
+someNewData = newArr(someNewData)
+
+console.log(someNewData);
+
+
+// TASK 8
+
+console.log("Task 8");
+
+var data = [334, 233, 212, 199, 154, 122];
+
+function subtraction(arr) {
+
+  var newArray = arr.map(function(element){
+
+    if (arr.indexOf(element) !== 0) {
+
+      return element - arr[arr.indexOf(element)-1];
+
+    } else return element;
+  });
+
+  return newArray;
+}
+
+var newArr = subtraction(data);
+
+console.log(newArr);
+
+// Other way
+
+//var arr = [];
+// function reduce(reducedArr){
+//  for (var i = 0; i < reducedArr.length; i++) {
+//    arr[i] = reducedArr[i] - reducedArr[i - 1]
+//  }
+//  return arr;
+// }
+// reduce(data);
+// console.log(arr);
+
+// TASK 9
+
+console.log("Task 9");
+
+var students = [
+  {
+     name: "Jim",
+     avgGrade: 8.5556
+  },
+  {
+     name: "Mike",
+     avgGrade: 8.5492
+  },
+  {
+     name: "Anna",
+     avgGrade: 8.9322
+  },
+  {
+     name: "Jack",
+     avgGrade: 8.6111
+  }
+]
+
+var averageGrade = 8.5;
+
+function bestOf(arr, avg) {
+
+  var newArray = arr.filter(function(element){
+
+    return element.avgGrade > avg;
+
+  });
+
+  newArray.forEach(function(element) {
+
+    element.avgGrade = element.avgGrade.toFixed(2);
+
+  });
+
+  return newArray;
+}
+
+var goodStudents = bestOf(students, averageGrade);
+
+console.log(goodStudents);
+
+
+//Other way
+
+// var someArr = [];
+
+// function grade (student, grade) {
+//  for (var i = 0; i < student.length; i++) {
+//    if (student[i].avgGrade > grade) {
+//      someArr[i] = student[i].name + "`s" + " average grade is: " +  student[i].avgGrade.toFixed(2);
+//    } 
+//    else
+//    {
+//      console.log("There are no students with avg grade above 8.5");
+//    }
+
+//  }
+//  return someArr;
 // }
 
-check(person_two);
-console.log(person_two);
-
-// //TASK 3
-
-console.log("TASK 3")
-
-var students = [ 
-   {
-       name: 'Mike',
-       age: 28,
-       passed: false
-   },
-   {
-       name: 'Anna',
-       age: 23,
-       passed: true
-   },
-   {
-       name: 'Jack',
-       age: 32,
-       passed: true
-   }
-];
-
-function checked(object){
-  for (var i = 0; i < object.length; i++) {
-    if (object[i].passed) 
-    {
-      console.log(object[i].name + " passed the exam.");
-    } 
-    else 
-    {
-      console.log(object[i].name + " didnt pass the exam.");
-    }
-  }
-}
-
-checked(students);
-
-// //TASK 4
-
-console.log("TASK 4")
-
-var students = [ 
-   {
-       name: 'Mike',
-       age: 28,
-       passed: false
-   },
-   {
-       name: 'Anna',
-       age: 23,
-       passed: true
-   },
-   {
-       name: 'Jack',
-       age: 32,
-       passed: true
-   },
-];
-
-function repack(object){
-
-  var names = [];
-  var ages = [];
-  var passed = [];
-
-  for (var i = 0; i < object.length; i++) {
-
-    names[i] = students[i].name;
-    ages[i] = students[i].age;
-    passed[i] = students[i].passed;
-  }
-
-  console.log(names);
-  console.log(ages);
-  console.log(passed);
-}
-
-repack(students);
-
-// //TASK 5
-
-console.log("TASK 5")
-
-var person = {
-  name : "Jack",
-  age : 32,
-  married: true
-}
-
-var otherObject = Object.create(person);
-
-console.log(otherObject.age);
-
-//TASK 6
-
-console.log("TASK 6")
-
-var person = {
-  name: "Mike",
-  age: 28,
-  married: true,
-
-    printData: function(){
-    console.log("Name is " + this.name)
-    console.log("Age is " + this.age)
-  }
-};
-
-person.printData();
-
-// //TASK 7
-
-console.log("TASK 7")
-
-var person = {};
-
-person.printData = function(name, age, married){   
-  this.name = name;
-  this.age = age;
-  this.married = married;   
-};
-
-person.printData("Peter", 25, true);
-
-console.log(person);
+// grade(students, averageGrade);
+// console.log(someArr);
