4779af14ecded178e7341e526ebeaa10ffb79069
jovanpavlovic <jovan94pavlovic@gmail.com>
2020-01-19 23:19:48 +0100

    BOM

--- js/main.js
+++ js/main.js
@@ -1,107 +1,128 @@
-//TASK 1 
+// TASK 1
 
-console.log("Task 1");
+// function available(){
+//  if (window.navigator.onLine) {
+//    alert("Browser is online.")
+//  }
+//  else
+//  {
+//    alert("Browser is offline.")
+//  }
+// }
 
-function Student(name,surname,age,avg){
-  this.name = name;
-  this.surname = surname;
-  this.age = age;
-  this.avg = avg;
-}
-
-var mike = new Student("Mike", "Williams", 25, 7.8);
-var peter = new Student("Peter", "Johnson", 23, 8.9);
-
-console.log(mike, peter);
+// available();
 
 //TASK 2
 
-console.log("Task 2");
+// function reload(){
 
-function FootbalPlayer(name,age,goals,yellowCards){
-  this.name = name;
-  this.age = age;
-  this.goals = goals;
-  this.yellowCards = yellowCards;
-}
+//  window.location.reload();
 
-FootbalPlayer.prototype.team = "Juventus";
+// }
 
-var cristiano = new FootbalPlayer("Cristiano",34, 10, 0);
-var paulo = new FootbalPlayer("Paulo",26, 8, 3);
-var matthijs = new FootbalPlayer("Matthijs",20, 1, 2);
-var adrien = new FootbalPlayer("Adrien",24, 0, 2);
-var merih = new FootbalPlayer("Merih",21, 1, 1);
+// reload();
 
-console.log(cristiano,paulo,matthijs,adrien,merih);
+// TASK 3
 
-//TASK 3
+// console.log("Task 3");
 
-console.log("Task 3")
+// function goToGoogle(){
 
-function Player(name,age,gender,adress){
-  this.name = name;
-  this.age = age;
-  this.gender = gender;
-}
+//  setTimeout(function(){
+//    window.location.href = "http://google.com";
+//  }, 3000);
+// }
 
-Player.prototype.team = "Juventus";
+// goToGoogle();
 
-var footbalPlayers = [
-["Cristiano",34, 10, 0],
-["Paulo",26, 8, 3],
-["Matthijs",20, 1, 2],
-["Adrien",24, 0, 2],
-["Merih",21, 1, 1],
-];
+// TASK 4
 
-function write(footbalPlayers){
-  var newplayers = [];
-  for (var i = 0; i < footbalPlayers.length; i++) {
-    newplayers = new Player(footbalPlayers[i][0], footbalPlayers[i][1], footbalPlayers[i][2], footbalPlayers[i][3]);
-    console.log(newplayers);
-  }
-}
-write(footbalPlayers);
+// var num = 0;
 
-//TASK 4
+// function printNumber(){
+//  num++;
+//  console.log("Number value is "+ num)
 
-console.log("TASK 4");
+//  if (num>=15) {
+//    clearInterval(int);
+//  }
+// }
 
-function Method(firstNum, secondNum, method) {
-  this.firstNum = firstNum;
-  this.secondNum = secondNum;
-  this.result = function() {
-    return method(this.firstNum, this.secondNum);
-  };
-}
+// var int = setInterval(printNumber, 1000)
 
-function add(firstNum, secondNum) {
-  var x = firstNum + secondNum;
-  return x;
-}
-function substract(firstNum, secondNum) {
-  var x = firstNum - secondNum;
-  return x;
-}
-function multiply(firstNum, secondNum) {
-  var x = firstNum * secondNum;
-  return x;
-}
-function divide(firstNum, secondNum) {
-  var x = firstNum / secondNum;
-  return x;
-}
+// TASK 5
 
+// function randomNumber(){
+//  console.log(Math.round(Math.random()*10));
+// }
 
-var addition = new Method(11, 9, add);
-console.log("11 + 9 = " + addition.result());
+// randomNumber();
 
-var substraction = new Method(8, 7, substract);
-console.log("8 - 7 = " + substraction.result());
+// TASK 6
 
-var multiplication = new Method(9, 5, multiply);
-console.log("9 * 5 = " +multiplication.result());
+// var users = [
+//  {
+//    name: "John",
+//    age: 46,
+//    status: "inactive"
+//  },
+//  {
+//    name: "Rachel",
+//    age: 32,
+//    status: "inactive"
+//  },
+//  {
+//    name: "Mike",
+//    age: 63,
+//    status: "inactive"
+//  },
+//  {
+//    name: "Michael",
+//    age: 15,
+//    status: "inactive"
+//  },
+//  {
+//    name: "Peter",
+//    age: 28,
+//    status: "inactive"
+//  }
+// ];
 
-var division = new Method(45, 5, divide);
-console.log("45 / 5  = " +  division.result());
+// var userCheck = prompt("Enter your name:");
+// var newUser;
+
+// newUser = users.filter(function(newUser){
+
+//  if (userCheck.toLowerCase() === newUser.name.toLowerCase()) {
+
+//    console.log("Hi, " + newUser.name + "!" + " You`re logged in.");
+
+//    newUser.status = "active";
+
+//    return newUser.name;
+//  } 
+// });
+
+//  if (newUser.length === 0) {
+
+//    console.log("User with name " + "*" + userCheck.toUpperCase() + "*"  + " does not exist.");
+//  } 
+
+//  else 
+
+//  {
+
+//    this.localStorage.setItem("newUser", newUser[0].name);
+
+//    setTimeout(function(){
+
+//        this.localStorage.removeItem("newUser");
+
+//        console.log(newUser.name + "You`re logged out!");
+
+//        newUser[0].status = "inactive";
+
+//        newUser = undefined;
+
+//    }, 60000);
+//  }
