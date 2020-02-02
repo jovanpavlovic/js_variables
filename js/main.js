a5b99a8ad75d67c4efcc09457696ee8893229563
jovanpavlovic <jovan94pavlovic@gmail.com>
2020-02-02 23:12:33 +0100

    Dog breeds search app

--- js/main.js
+++ js/main.js
@@ -1,85 +1,56 @@
-var list = document.querySelector('#movie-list ul');
-var forms = document.forms;
+// var opt = document.getElementById("dogBreed");
+// var sel = opt.options[opt.selectedIndex].value;
 
-// hide movies - show series only...show items with class action
-var filterSeries = document.querySelectorAll(".action");
-var seriesOnly = document.querySelector('#series');
+// function getData() {
+//     var newRequest = new XMLHttpRequest();
 
-seriesOnly.addEventListener('change', function(e){
-filterSeries.forEach(function(serie){
-  if(seriesOnly.checked){
-    serie.style.display = "none";
-  } else {
-    serie.style.display = "flex";
-    serie.style.justifyContent = "space-between";
-    }
-  });
-});
+//     newRequest.open("GET","https://dog.ceo/api/breeds/image/random");
 
-// del
-list.addEventListener('click', function (e) { //when soimeone click ul we need to check where the user clicked, well do that with target.className
-  if(e.target.className == 'del'){
-    var li = e.target.parentElement;         //parent element of the button that we clicked
-   list.removeChild(li);           // grab Node above, instead of li.parentNode we can use var list
-  }
-});
+//     newRequest.onload = function() {
+//         console.log(newRequest);
+//     }
 
-  // add
-  var addForm = forms['add-movie'];
-  addForm.addEventListener('submit', function(e){
-  e.preventDefault();
+//     newRequest.send();
+//  }  
 
-  // create
-  var value = addForm.querySelector('input[type="text"]').value;
-  var li = document.createElement('li');
-  var movieName = document.createElement('span');
-  var delBtn = document.createElement('span');
+//  getData();
 
-  movieName.textContent = value;
-  delBtn.textContent = 'Delete';
+var select;
 
-  movieName.classList.add('name');
-  delBtn.classList.add('del');
+var fetchDoggoBreeds = async () => {
 
-  li.appendChild(movieName);
-  li.appendChild(delBtn);
-  list.appendChild(li);
-});
+  var data = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json());
+  dogSelect(data);
+}
 
+var dogSelect = function (breeds) {
+  select = document.querySelector('.breed');
+  var breedOpt = breeds.map(function (breed) {
+    var option = document.createElement('option');
+    option.text = breed.name;
+    option.value = breed.id;
+    return option;
+  })
 
+  breedOpt.forEach( function (breedOption) {
+    select.appendChild(breedOption);
+  })
+}
 
-var search = document.forms['search-movies'].querySelector('input'); //refenrece to the search input
+var image = function (imageUrl) {
+  document.querySelector('#dogImg').setAttribute('src', imageUrl);
 
-search.addEventListener('keyup', function(e) {                       //keyp event is every letter that we type in, we we want to compare it to values that we have
-  var term = e.target.value.toLowerCase();                           //we want to grab value of input field
-  var movies = list.getElementsByTagName('li');                      
-  Array.from(movies).forEach(function (movie) {                      //movies return htmlcollectio so we need to make an array from it so we can cycle through them
-    var title = movie.firstElementChild.textContent;                 //we want to grab spans text content
-    if(title.toLowerCase().indexOf(e.target.value) != -1){           // we get index of term(position in a string). For instantce when we type or postion of a string in "Worlds apart" will be 1 if we type something that doiesnt existiindexOf(e.target.value) will return -1
-      movie.style.display = 'inital';
-    } else {
-      movie.style.display = 'none';
-    }
-  });
-});
+}
 
+var getDogByBreed = async function (breedId) {
+  var [data] = await fetch('https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=' + breedId).then((data => data.json()));
+  var {url: imageUrl, breeds} = data;
+  console.log(breeds[0]);
+  image(imageUrl);
+}
 
-// function validateForm() {
-//   var x = document.forms["y"]["y"].value;
-//   if (x == "") {
-//     alert("Name must be filled out");
-//     return false;
-//   }
-// }
+var changeDog = function() {
+  getDogByBreed(event.target.value);
+}
 
-
-//panel
-
-// var flip = document.querySelectorAll(".flip");
-// var panel = document.querySelectorAll('.panel');
-
-//  for(var i = 0; i < flip.length; i++) {
-//     var panel = document.querySelectorAll('.panel');
-//        flip[i].addEventListener("click", function(e){
-//   });
-//  }
+fetchDoggoBreeds();
