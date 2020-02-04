854b92e1450cb11cb8c044db207b7bd802192130
jovanpavlovic <jovan94pavlovic@gmail.com>
2020-02-04 23:11:17 +0100

    Simple youtube app with youtube API

--- js/main.js
+++ js/main.js
@@ -1,56 +1,93 @@
-// var opt = document.getElementById("dogBreed");
-// var sel = opt.options[opt.selectedIndex].value;
 
-// function getData() {
-//     var newRequest = new XMLHttpRequest();
+var searchButton = document.querySelector(".search button");
+var key = "AIzaSyDL91x1vwiL9_NuCW7vsMPJaZk5ArMQiAQ";
+var videoList = document.querySelector(".video-list");
+var videoPreview = document.querySelector(".video-preview");
 
-//     newRequest.open("GET","https://dog.ceo/api/breeds/image/random");
+function onSearch() {
+	var searchFiled = document.querySelector(".search input");
 
-//     newRequest.onload = function() {
-//         console.log(newRequest);
-//     }
-
-//     newRequest.send();
-//  }  
-
-//  getData();
-
-var select;
-
-var fetchDoggoBreeds = async () => {
-
-  var data = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json());
-  dogSelect(data);
+	searchFiled.value.trim() && getVideos(searchFiled.value);
+	searchFiled.value = '';
 }
 
-var dogSelect = function (breeds) {
-  select = document.querySelector('.breed');
-  var breedOpt = breeds.map(function (breed) {
-    var option = document.createElement('option');
-    option.text = breed.name;
-    option.value = breed.id;
-    return option;
-  })
+function getVideos(searchValue) {
+	var request = new XMLHttpRequest();
 
-  breedOpt.forEach( function (breedOption) {
-    select.appendChild(breedOption);
-  })
+	console.log(searchValue);
+
+	request.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + searchValue +'&key=' + key);
+
+	request.onload = function(){
+		console.log(request.responseText);
+		listVideos(JSON.parse(request.responseText).items); 
+	}
+
+	request.send();
 }
 
-var image = function (imageUrl) {
-  document.querySelector('#dogImg').setAttribute('src', imageUrl);
+function getRelatedVideos(videoId) {
+	var request = new XMLHttpRequest();
+
+	request.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=snippet&\
+	maxResults=6&type=video&relatedToVideoId=' + videoId +'&key=' + key);
+
+	request.onload = function(){
+		listVideos(JSON.parse(request.responseText).items, "related");
+	}
+
+	request.send();
+}
+
+
+
+function listVideos(videos) {
+
+	videoList.innerHTML = "";
+
+	videos.forEach(function(video) {
+		addVideo(video);
+	})
+	
+}
+
+
+function addVideo(videoData) {
+	var videoElement = document.createElement("div");
+	var img = '<img src="' + videoData.snippet.thumbnails.medium.url +'">';
+	var title = '<h3>' + videoData.snippet.title + '</h3>';
+	var desc ='<div>' + videoData.snippet.description + '</div>';
+	
+
+	videoElement.innerHTML ='<div id="img-wrap">'+  img + '</div>' + '<section>' + title + desc + '</section>';
+	videoList.appendChild(videoElement);
+
+	console.log(videoElement);
+
+	videoElement.querySelectorAll("h3, img").forEach(function(element) {
+		element.addEventListener("click", function() {
+			openVideo(videoData.id.videoId)
+		})
+	})
 
 }
 
-var getDogByBreed = async function (breedId) {
-  var [data] = await fetch('https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=' + breedId).then((data => data.json()));
-  var {url: imageUrl, breeds} = data;
-  console.log(breeds[0]);
-  image(imageUrl);
+function openVideo(id) {
+	videoPreview.innerHTML = '<iframe width="100%" height="500" src="https://www.youtube.com/embed/'+ id +'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+	videoPreview.style.margin = "20px";
+	videoPreview.style.borderBottom = "3px solid #282828";
+
+	var closePreview = document.createElement("button")
+	closePreview.textContent = "Close perview";
+
+	videoPreview.appendChild(closePreview);
+
+	closePreview.addEventListener("click", function(){
+		videoPreview.style.display = "none";
+	})
+
 }
 
-var changeDog = function() {
-  getDogByBreed(event.target.value);
-}
+// init
 
-fetchDoggoBreeds();
+searchButton.addEventListener("click", onSearch)
